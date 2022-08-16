8ae34a4210686cace6143cb4f4d87695dcb44597
jinsu291 <wlstn291@naver.com>
2022-08-16 22:54:14 +0900

    stream 방식의 구구단

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -3,6 +3,9 @@ package com.ll.exam.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class MainController {
 
@@ -58,5 +61,23 @@ public class MainController {
         increaseNo++;
 
         return increaseNo;
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String showGugudan(Integer dan, Integer limit) {
+
+        if(limit == null){
+            limit= 9;
+        }
+
+        if(dan == null){
+            dan = 9;
+        }
+
+        Integer finalDan = dan;
+        return IntStream.rangeClosed(1, limit)
+                       .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
+                       .collect(Collectors.joining("<br>\n"));
     }
 }
