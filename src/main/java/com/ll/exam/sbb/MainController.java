115d159fde234bf8dcd96406f0837dd03d4966fa
jinsu291 <wlstn291@naver.com>
2022-08-16 23:04:02 +0900

    서블릿 방식으로 요청과 응답 비교

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -3,6 +3,9 @@ package com.ll.exam.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -46,6 +49,15 @@ public class MainController {
     @ResponseBody
     public int   showPlus(int a, int b) {
         return a + b;
+    }
+
+    @GetMapping("/plus2")
+    @ResponseBody
+    public void    showPlus2(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int a = Integer.parseInt(req.getParameter("a"));
+        int b = Integer.parseInt(req.getParameter("b"));
+
+        resp.getWriter().append(a + b + "");
     }
 
     @GetMapping("/minus")
