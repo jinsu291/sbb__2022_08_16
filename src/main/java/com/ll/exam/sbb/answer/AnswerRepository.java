11529667880cd9711dee86f75591d3d62d1382eb
jinsu291 <wlstn291@naver.com>
2022-08-19 01:18:56 +0900

    도메인 별로 패키지 분리

--- src/main/java/com/ll/exam/sbb/answer/AnswerRepository.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerRepository.java
@@ -1,4 +1,4 @@
-package com.ll.exam.sbb;
+package com.ll.exam.sbb.answer;
 
 import com.ll.exam.sbb.base.RepositoryUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,18 +6,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> ,RepositoryUtil {
-    Question findBySubject(String subject);
-
-    Question findBySubjectAndContent(String subject, String content);
-
-    List<Question> findBySubjectLike(String s);
+public interface AnswerRepository extends JpaRepository<Answer, Integer> , RepositoryUtil {
 
     @Transactional
     @Modifying
-    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+    @Query(value = "ALTER TABLE answer AUTO_INCREMENT = 1", nativeQuery = true)
     void truncate();   //이거 지우면 안됨, truncateTable 하면 자동으로 이게 실행됨
 
 }
--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -1,5 +1,7 @@
 package com.ll.exam.sbb;
 
+import com.ll.exam.sbb.question.Question;
+import com.ll.exam.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
