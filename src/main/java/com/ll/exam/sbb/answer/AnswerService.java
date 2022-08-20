4b92785c51bc1d46d6d93b7ef457418299a9b8d3
jinsu291 <wlstn291@naver.com>
2022-08-20 17:44:47 +0900

    답변 저장 코드 개선

--- src/main/java/com/ll/exam/sbb/answer/AnswerService.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerService.java
@@ -15,7 +15,8 @@ public class AnswerService {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
-        answer.setQuestion(question);
+        question.addAnswer(answer);
+
         answerRepository.save(answer);
     }
 }
