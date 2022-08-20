44ee43ed1e4f852d5e1a26a3dc4c1a9ac0d1dc95
jinsu291 <wlstn291@naver.com>
2022-08-20 17:26:11 +0900

    답변 저장 처리

--- src/main/java/com/ll/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -14,12 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String detail(Model model, @PathVariable int id, String content) {
         Question question = this.questionService.getQuestion(id);
 
         // 답변 등록 시작
+        answerService.create(question, content);
         // 답변 등록 끝
 
         return "redirect:/question/detail/%d".formatted(id);
