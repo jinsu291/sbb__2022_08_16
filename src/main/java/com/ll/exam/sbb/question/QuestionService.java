5feb4b23dbe5407bbf5513e8378f35a273da731c
jinsu291 <wlstn291@naver.com>
2022-08-20 17:42:01 +0900

    QuestionService.getQuestion 코드 개선

--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -16,13 +16,8 @@ public class QuestionService {
         return this.questionRepository.findAll();
     }
 
-    public Question getQuestion(int id) throws DataNotFoundException {
-        Optional<Question> oq = questionRepository.findById(id);
-
-        if (oq.isPresent()) {
-            return oq.get();
-        }
-
-        throw new DataNotFoundException("question not found");
+    public Question getQuestion(int id) {
+        return questionRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("no %d question not found,".formatted(id)));
     }
 }
