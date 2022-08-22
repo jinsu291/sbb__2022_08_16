273598f1340f968c8ffee06c0a674ec752d6f196
jinsu291 <wlstn291@naver.com>
2022-08-22 10:36:17 +0900

    폼체크 개선

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -52,13 +52,20 @@ public class QuestionController {
 
     @PostMapping("/create")
     public String questionCreate(Model model, QuestionFrom questionFrom) {
+        boolean hasError = false;
+
         if (questionFrom.getSubject() == null || questionFrom.getSubject().trim().length() == 0) {
-            model.addAttribute("errorMsg", "제목 좀...");
-            return "question_form";
+            model.addAttribute("subjectErrorMsg", "제목 좀...");
+            hasError = true;
         }
 
         if (questionFrom.getContent() == null || questionFrom.getContent().trim().length() == 0) {
-            model.addAttribute("errorMsg", "내용 좀...");
+            model.addAttribute("contentErrorMsg", "내용 좀...");
+            hasError = true;
+        }
+
+        if (hasError) {
+            model.addAttribute("questionFrom", questionFrom);
             return "question_form";
         }
 
@@ -66,7 +73,6 @@ public class QuestionController {
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
-
 
 
 
