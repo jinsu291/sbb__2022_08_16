682a7268082052fa3cbac94a2527de7be552d4d8
jinsu291 <wlstn291@naver.com>
2022-08-23 11:13:18 +0900

    한 페이지에서 10개씩 나오도록

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -140,7 +140,7 @@ public class QuestionRepositoryTests {
 
     @Test
     void createManySampleData(){
-        boolean run = true;
+        boolean run = false;
 
         if(run == false) return;
 
